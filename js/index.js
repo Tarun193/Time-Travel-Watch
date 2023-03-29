35eaef50d6eef4103aeaadc0a4d0ba6427f3cc0e
Your Name <you@example.com>
2023-03-29 09:53:11 -0400

    Added in cross icon in menu for mobile and added some event listeners

--- js/index.js
+++ js/index.js
@@ -1,6 +1,25 @@
 const HamBurger = document.getElementById("HamBurger");
+const Cross = document.getElementById("Cross-Logo");
+const NavLinks = document.querySelectorAll("header nav ul a");
+
+NavLinks.forEach((link) => {
+  console.log(link);
+  link.addEventListener("click", HidePhoneMenu);
+});
 HamBurger.addEventListener("click", () => {
+  const cross = document.getElementById("Cross-Logo");
   const nav = document.getElementById("Nav-Bar");
-  console.log(nav);
+  cross.style.display = "block";
   nav.setAttribute("class", "longNav");
 });
+
+Cross.addEventListener("click", HidePhoneMenu);
+
+function HidePhoneMenu(obj) {
+  const nav = document.getElementById("Nav-Bar");
+  if (nav.getAttribute("class") === "longNav") {
+    const cross = document.getElementById("Cross-Logo");
+    cross.style.display = "none";
+    nav.removeAttribute("class");
+  }
+}
