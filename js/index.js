5c78c8cd57a2af973d3c9078c5a829055518e029
Your Name <you@example.com>
2023-04-02 17:14:43 -0400

    Complete the home page

--- js/index.js
+++ js/index.js
@@ -1,25 +1,30 @@
+// Getting all the required Elements
 const HamBurger = document.getElementById("HamBurger");
 const Cross = document.getElementById("Cross-Logo");
 const NavLinks = document.querySelectorAll("header nav ul a");
+const nav = document.getElementById("Nav-Bar");
 
+if (nav.getAttribute("class") === "longNav") {
+  // Add an event listener to the scroll event
+}
 NavLinks.forEach((link) => {
-  console.log(link);
   link.addEventListener("click", HidePhoneMenu);
 });
+
 HamBurger.addEventListener("click", () => {
   const cross = document.getElementById("Cross-Logo");
-  const nav = document.getElementById("Nav-Bar");
   cross.style.display = "block";
+  document.body.style.overflow = "hidden";
   nav.setAttribute("class", "longNav");
 });
 
 Cross.addEventListener("click", HidePhoneMenu);
 
 function HidePhoneMenu(obj) {
-  const nav = document.getElementById("Nav-Bar");
   if (nav.getAttribute("class") === "longNav") {
     const cross = document.getElementById("Cross-Logo");
     cross.style.display = "none";
+    document.body.style.overflow = "visible";
     nav.removeAttribute("class");
   }
 }
